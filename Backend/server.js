9c712ee8982eb0f1fb663d9efd9b39dd88696ec7
tushardacoder <tusharbasak071@gmail.com>
2026-03-01 16:59:04 +0600

    cors added

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+app.use(cors()); // allow all origins
+
+// Allow all origins (development)
 
 app.use(express.json());
 
